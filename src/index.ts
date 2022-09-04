f582c2ea217f6404812e86d0eedf57650e8c5300
Patrick Kelly <patomation@gmail.com>
2022-09-03 21:56:07 -0700

    feat(index): added config support

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,92 @@
 export interface Clunk {
-  [key: string]: string | boolean | number
-}
-
-enum Flag {
-  SHORT,
-  LONG,
-}
-
-function whatFlag(text: string = ''): Flag | null {
-  const characters = Array.from(text)
-  const DASH = '-'
-  const isSingleDash = characters[0] === DASH
-  const isDoubleDash = characters[1] === DASH
-  return isDoubleDash ? Flag.LONG : isSingleDash ? Flag.SHORT : null
-}
-
-function removeDashes(text: string): string {
-  return text.replace(/-/g, '')
-}
-
-function getValue(text: string): string | boolean | number {
-  return text && whatFlag(text) === null
-    ? text === 'false'
-      ? false
-      : text === 'true'
-      ? true
-      : isNaN(Number(text || ''))
-      ? text
-      : Number(text)
-    : true
-}
-
-export function parseArgs(argv: string[]): Clunk {
-  const hash: Clunk = {}
-  const [, , ...rest] = argv
-  for (let i = 0; i < rest.length; i++) {
-    const text = rest[i]
-    const nextText = rest[i + 1]
-    const flagKind = whatFlag(text)
-
-    if (flagKind === Flag.SHORT) {
-      // Handle multiple short combined keys
-      const keys = Array.from(removeDashes(text).toLowerCase())
-      keys.forEach((key, i) => {
-        // The last one should get the text argument the first ones should be booleans
-        hash[key] = keys.length - 1 === i ? getValue(nextText) : true
-      })
-    } else if (flagKind === Flag.LONG) {
-      const key = removeDashes(text).toLowerCase()
-      hash[key] = getValue(nextText)
-    } else {
-      const previousFlag = whatFlag(rest[i - 1])
-      if (i === 0) {
-        hash.prefix = text
-      } else if (previousFlag === null && rest.length - 1 === i) {
-        hash.suffix = text
-      }
-    }
+  inputs: string[]
+  flags: {
+    [key: string]: string | boolean | number | null
   }
-  return hash
 }
 
-export function clunk(): Clunk {
-  return parseArgs(process.argv)
+interface ConfigItem {
+  description?: string
+  type: typeof Boolean | typeof String | typeof Number
+  alias?: string
+}
+export interface Config {
+  [name: string]: ConfigItem
+}
+
+function getConfigFlagByAlias(config: Config, alias: string): string | null {
+  let matchingFlag: string | null = null
+  Object.entries(config).find(([flag, configItem]) => {
+    const isMatch: boolean = configItem.alias === alias
+    if (isMatch) matchingFlag = flag
+    return isMatch
+  })
+  return matchingFlag
+}
+
+function toBoolean(text: string): boolean | null {
+  return text === 'true' ? true : text === 'false' ? false : null
+}
+
+export function parser(
+  [item, ...rest]: string[],
+  config: Config,
+  inputs: Clunk['inputs'] = [],
+  flags: Clunk['flags'] = {}
+): Clunk {
+  const [char1, char2] = Array.from(item)
+  const isLongFlag = char2 === '-'
+  const isShortFlag = !isLongFlag && char1 === '-'
+  const flag = item.replace(/-/g, '')
+  const isNextItemFlag: boolean = Array.from(rest[0] || '')[0] === '-'
+  if (isLongFlag) {
+    const flagType = config[flag]?.type
+    if (rest[0]) {
+      const nextBoolean = toBoolean(rest[0])
+      if (isNextItemFlag) {
+        flags[flag] = true
+      } else {
+        if (flagType === Boolean) {
+          flags[flag] = nextBoolean !== null ? nextBoolean : true
+          if (nextBoolean !== null && rest[0]) rest.shift()
+        } else if (flagType === String) {
+          flags[flag] = String(rest[0])
+          rest.shift()
+        } else if (flagType === Number) {
+          flags[flag] = Number(rest[0])
+          rest.shift()
+        } else if (nextBoolean !== null) {
+          flags[flag] = nextBoolean
+          rest.shift()
+        } else if (flag.length > 1) {
+          flags[flag] = rest[0]
+          rest.shift()
+        } else if (flag.length === 1 && rest[0] && rest[1]) {
+          flags[flag] = rest[0]
+          rest.shift()
+        } else {
+          flags[flag] = true
+        }
+      }
+    } else {
+      flags[flag] = true
+    }
+  } else if (isShortFlag) {
+    const aliases = Array.from(item.replace(/-/g, '')).map(
+      (alias) => `--${getConfigFlagByAlias(config, alias) || alias}`
+    )
+    // Send them back through
+    rest = [...aliases, ...rest]
+  } else {
+    inputs.push(item)
+  }
+  const result =
+    rest.length > 0 ? parser(rest, config, inputs, flags) : { inputs, flags }
+
+  return result
+}
+
+export function clunk(config?: Config): Clunk {
+  const [, , ...rest] = process.argv
+  return parser(rest, config || {})
 }
