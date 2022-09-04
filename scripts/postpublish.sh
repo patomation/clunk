e1aee7c94d4e49f0583e8890b375065f8c68166e
Patrick Kelly <patomation@gmail.com>
2022-09-04 12:24:33 -0700

    feat(scripts): added prepublish and postpublish scripts

---
+++ scripts/postpublish.sh
@@ -0,0 +1,2 @@
+cp package-backup.json package.json
+rm package-backup.json
---
+++ scripts/prepublish.sh
@@ -0,0 +1,4 @@
+cp package.json package-backup.json
+npm pkg delete scripts
+npm pkg delete ava
+npm pkg delete devDependencies
